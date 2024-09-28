d731ee29add8a8541fc30de5491e3a4c89a1059a
vwh <vwhe@proton.me>
2024-09-28 08:07:55 +0300

    feat: added tsx, removed nodemon, small improvments

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	format: ["cjs", "esm"],
 	entry: ["./src/index.ts"],
+	outDir: "./dist",
 	dts: true,
 	shims: true,
 	skipNodeModulesBundle: true,
 	clean: true,
+	minify: true,
 });
